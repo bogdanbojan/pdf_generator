9662cb80d969428c6c258ef54d69c1c448829e44
bogdanbojan <71257990+bogdanbojan@users.noreply.github.com>
2021-03-26 13:57:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 
 
 import sys
-sys.path.append(r"C:\Users\bbogd\AppData\Local\Programs\Python\Python39\Lib\site-packages")
+sys.path.append(r"C:\Users\bbogd\AppData\Local\Programs\Python\Python39\Lib\site-packages") #??
 sys.path.append(r"C:\Users\bbogd\AppData\Local\Programs\Python\Python39\Lib")
 sys.path.append(r"C:\Users\bbogd\AppData\Local\Programs\Python\Python39\Lib\npath.py")
 from selenium import webdriver
@@ -14,36 +14,71 @@ import examples._psd_files as psd  # Import from examples.
 PSD_FILE = psd.get_psd_files()
 
 
-def hide_all_layers(layers):
-    for layer in layers:
-        layer.visible = False
 
 
-def main():
-    psd_file = PSD_FILE["export_layers_as_png.psd"]
-    with Session(psd_file, action="open") as ps:
-        doc = ps.active_document
-        options = ps.PDFSaveOptions()
-        layers = doc.artLayers
+class Export:
 
-        count = 1
-
+    def hide_all_layers(self,layers):
         for layer in layers:
-            hide_all_layers(layers)
-            layer.visible = True
-            layer_path = os.path.join(doc.path)
-            print(layer_path)
+            layer.visible = False
 
-            layer.name = str(count)
-            count += 1
-            print(layer.name)
+    def export_pdf(self):
+        image_path = os.path.join(self.layer_path, f"{self.layer.name}.pdf")
+        self.doc.saveAs(image_path, self.options, True)
 
-            if not os.path.exists(layer_path):
-                os.makedirs(layer_path)
-            image_path = os.path.join(layer_path, f"{layer.name}.pdf")
-            doc.saveAs(image_path, options, True)
-        ps.alert("Task done!")
-        ps.echo(doc.activeLayer)
+    def setup_layers_for_export(self):
+        for self.layer in self.layers: #??
+            self.hide_all_layers(self.layers)
+            self.layer.visible = True
+            self.layer_path = os.path.join(self.doc.path)
+            print(self.layer_path)
+
+            self.layer.name = str(self.count)
+            self.count += 1  # ??
+            print(self.layer.name)
+
+            if not os.path.exists(self.layer_path):
+                os.makedirs(self.layer_path)
+
+    def setup_session(self):
+        psd_file = PSD_FILE["export_layers_as_png.psd"]
+        with Session(psd_file, action="open") as ps:
+            self.doc = ps.active_document
+            self.options = ps.PDFSaveOptions()
+            self.layers = self.doc.artLayers
+            self.count = 1
+
+            self.setup_layers_for_export()
+            self.export_pdf()
+
+            ps.alert("Task done!")
+            ps.echo(self.doc.activeLayer)
+
+#
+# def export_layers_in_ascending_order():
+#     psd_file = PSD_FILE["export_layers_as_png.psd"]
+#     with Session(psd_file, action="open") as ps:
+#         doc = ps.active_document
+#         options = ps.PDFSaveOptions()
+#         layers = doc.artLayers
+#         count = 1
+#
+#         for layer in layers:
+#             hide_all_layers(layers)
+#             layer.visible = True
+#             layer_path = os.path.join(doc.path)
+#             print(layer_path)
+#
+#             layer.name = str(count)
+#             count += 1
+#             print(layer.name)
+#
+#             if not os.path.exists(layer_path):
+#                 os.makedirs(layer_path)
+#
+#             image_path = os.path.join(layer_path, f"{layer.name}.pdf")
+#             doc.saveAs(image_path, options, True)
+#
 
     driver = webdriver.Chrome(
         executable_path=r'C:\Program Files\Adobe\Adobe Photoshop 2020\Presets\Scripts\photoshop_python_integration\python\Lib\site-packages\multi_script_editor\examples\files\chromedriver.exe')
@@ -62,4 +97,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    export_layers_in_ascending_order()
