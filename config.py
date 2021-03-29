2ba7f940a39c8f9a66f0aacaea021253b4e92c30
Bogdan Bojan <bogdanbojan03@gmail.com>
2021-03-29 18:02:07 +0300

    updated config and cleaned code

---
+++ config.py
@@ -0,0 +1,10 @@
+"""
+If you are using MultiScript Editor make sure to set PYTHONPATH to Winexs' editor.
+You can use set PYTHONPATH=c:/users/username/myscripts
+
+Set paths according to your project!
+"""
+
+CHROME_WEBDRIVER = 'c:/users/username/project/chromedriver.exe'
+WEBSITE_PDF_CONVERTER = 'https://www.ilovepdf.com/merge_pdf'
+PDF_FILES = 'c:/users/username/project'
--- main.py
+++ main.py
@@ -1,46 +1,14 @@
 import os
-
-
-import sys
-sys.path.append(r"C:\Users\bbogd\AppData\Local\Programs\Python\Python39\Lib\site-packages") #??
-sys.path.append(r"C:\Users\bbogd\AppData\Local\Programs\Python\Python39\Lib")
-sys.path.append(r"C:\Users\bbogd\AppData\Local\Programs\Python\Python39\Lib\npath.py")
 from selenium import webdriver
-
 from photoshop import Session
-
-import examples._psd_files as psd  # Import from examples.
+import examples._psd_files as psd
+from config import CHROME_WEBDRIVER, PDF_FILES, WEBSITE_PDF_CONVERTER
 
 PSD_FILE = psd.get_psd_files()
 
 
-
-
-class Export:
-
-    def hide_all_layers(self,layers):
-        for layer in layers:
-            layer.visible = False
-
-    def export_pdf(self):
-        image_path = os.path.join(self.layer_path, f"{self.layer.name}.pdf")
-        self.doc.saveAs(image_path, self.options, True)
-
-    def setup_layers_for_export(self):
-        for self.layer in self.layers: #??
-            self.hide_all_layers(self.layers)
-            self.layer.visible = True
-            self.layer_path = os.path.join(self.doc.path)
-            print(self.layer_path)
-
-            self.layer.name = str(self.count)
-            self.count += 1  # ??
-            print(self.layer.name)
-
-            if not os.path.exists(self.layer_path):
-                os.makedirs(self.layer_path)
-
-    def setup_session(self):
+class PdfConverter:
+    def setup_files_for_web_upload(self):
         psd_file = PSD_FILE["export_layers_as_png.psd"]
         with Session(psd_file, action="open") as ps:
             self.doc = ps.active_document
@@ -48,53 +16,53 @@ class Export:
             self.layers = self.doc.artLayers
             self.count = 1
 
-            self.setup_layers_for_export()
-            self.export_pdf()
+            self.export_layers()
 
             ps.alert("Task done!")
             ps.echo(self.doc.activeLayer)
 
-#
-# def export_layers_in_ascending_order():
-#     psd_file = PSD_FILE["export_layers_as_png.psd"]
-#     with Session(psd_file, action="open") as ps:
-#         doc = ps.active_document
-#         options = ps.PDFSaveOptions()
-#         layers = doc.artLayers
-#         count = 1
-#
-#         for layer in layers:
-#             hide_all_layers(layers)
-#             layer.visible = True
-#             layer_path = os.path.join(doc.path)
-#             print(layer_path)
-#
-#             layer.name = str(count)
-#             count += 1
-#             print(layer.name)
-#
-#             if not os.path.exists(layer_path):
-#                 os.makedirs(layer_path)
-#
-#             image_path = os.path.join(layer_path, f"{layer.name}.pdf")
-#             doc.saveAs(image_path, options, True)
-#
+    def export_layers(self):
+        for self.layer in self.layers:
+            self.hide_all_layers(self.layers)
 
-    driver = webdriver.Chrome(
-        executable_path=r'C:\Program Files\Adobe\Adobe Photoshop 2020\Presets\Scripts\photoshop_python_integration\python\Lib\site-packages\multi_script_editor\examples\files\chromedriver.exe')
-    driver.get("https://www.ilovepdf.com/merge_pdf")
+            self.layer.visible = True
+            self.layer_path = os.path.join(self.doc.path)
+            print(self.layer_path)
 
-    button = driver.find_element_by_css_selector("input[type=file]")
+            self.layer.name = str(self.count)
+            self.count += 1
+            print(self.layer.name)
 
-    for image in range(1, len(layers) + 1):
-        button.send_keys(
-            r"C:\Program Files\Adobe\Adobe Photoshop 2020\Presets\Scripts\photoshop_python_integration\python\Lib\site-packages\examples\files\%s.pdf" % (
-                image))
+            self.export_pdf(self.layer)
 
-    # button.click()
-    convert_button = driver.find_element_by_xpath('//*[@id="processTask"]')
-    convert_button.click()
+            if not os.path.exists(self.layer_path):
+                os.makedirs(self.layer_path)
 
+    def hide_all_layers(self,layers):
+        for layer in layers:
+            layer.visible = False
+
+    def export_pdf(self,layer):
+        image_path = os.path.join(self.layer_path, f"{layer.name}.pdf")
+        self.doc.saveAs(image_path, self.options, True)
+
+
+
+
+    def download_converted_pdf_from_website(self):
+        self.driver = webdriver.Chrome(
+            executable_path=CHROME_WEBDRIVER)
+        self.driver.get(WEBSITE_PDF_CONVERTER)
+        self.button = self.driver.find_element_by_css_selector("input[type=file]")
+
+        self.upload_pdf_files_to_website()
+
+        convert_button = self.driver.find_element_by_xpath('//*[@id="processTask"]')
+        convert_button.click()
+
+    def upload_pdf_files_to_website(self):
+        for image in range(1, len(self.layers) + 1):
+            self.button.send_keys(PDF_FILES + '/%s.pdf' % (image))
 
 if __name__ == "__main__":
-    export_layers_in_ascending_order()
+    PdfConverter().setup_files_for_web_upload()
